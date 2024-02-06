3398214dceb7f700dc2d1f21cf132a3030ee8a98
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-02-06 18:31:44 +0530

    90% completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import GlobalState from "@/context";
+import NextAuthProvider from "@/auth-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextAuthProvider>
           <GlobalState>{children}</GlobalState>
+        </NextAuthProvider>
       </body>
     </html>
   );
